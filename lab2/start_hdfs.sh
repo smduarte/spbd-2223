fb876ac590bba5f53e4ae91f787ba97bf67af528
smduarte <smd@fct.unl.pt>
2022-09-27 22:56:55 +0100

    Update start_hdfs.sh

--- lab2/start_hdfs.sh
+++ lab2/start_hdfs.sh
@@ -28,3 +28,11 @@ cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
 
 $HADOOP_HOME/bin/hdfs namenode -format
+
+export HDFS_NAMENODE_USER="root"
+export HDFS_DATANODE_USER="root"
+export HDFS_SECONDARYNAMENODE_USER="root"
+export YARN_RESOURCEMANAGER_USER="root"
+export YARN_NODEMANAGER_USER="root"
+
+$HADOOP_HOME/sbin/start-dfs.sh
