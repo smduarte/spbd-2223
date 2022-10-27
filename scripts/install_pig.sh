69a1d7c0459060c6c78b0e4187f976730cedd814
smduarte <smd@fct.unl.pt>
2022-10-27 10:01:44 +0100

    Update install_pig.sh

--- scripts/install_pig.sh
+++ scripts/install_pig.sh
@@ -8,6 +8,6 @@ HADOOP_HOME=/usr/local/$HADOOP
 curl -s https://raw.githubusercontent.com/smduarte/spbd-2223/main/scripts/install_hadoop.sh | bash
 export PIG_HOME=/opt/pig-0.17.0
 mkdir -p $PIG_HOME
-cd $PIG_HOME
+cd /opt/
 wget -O /tmp/pig.tgz https://dlcdn.apache.org/pig/latest/pig-0.17.0.tar.gz
 tar xfz /tmp/pig.tgz
