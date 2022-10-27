2554a2652c1ce6801b8f5a1b051009b6eced32d4
smduarte <smd@fct.unl.pt>
2022-10-27 23:20:34 +0100

    Update install_pig.sh

--- scripts/install_pig.sh
+++ scripts/install_pig.sh
@@ -7,5 +7,5 @@ echo "export JAVA_HOME="`readlink -f /usr/bin/java | sed "s:bin/java::"`
 export PIG_HOME=/opt/pig-0.17.0
 mkdir -p $PIG_HOME
 cd /opt/
-wget -O /tmp/pig.tgz https://dlcdn.apache.org/pig/latest/pig-0.17.0.tar.gz
+wget -O /tmp/pig.tgz https://dlcdn.apache.org/pig/pig-0.16.0/pig-0.16.0.tar.gz
 tar xfz /tmp/pig.tgz
