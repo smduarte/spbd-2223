8829e35bd5dc311deb1af719541e37bf436ac5c4
smduarte <smd@fct.unl.pt>
2022-10-27 09:52:51 +0100

    Create install_pig.sh

---
+++ scripts/install_pig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#apt-get install -y openssh-server
+#/etc/init.d/ssh start
+
+HADOOP=hadoop-3.3.4
+HADOOP_HOME=/usr/local/$HADOOP
+
+curl -s https://raw.githubusercontent.com/smduarte/spbd-2223/main/scripts/install_hdfs.sh
+export PIG_HOME=/opt/pig-0.17.0
+mkdir -p $PIG_HOME
+cd $PIG_HOME
+wget -O /tmp/pig.tgz https://dlcdn.apache.org/pig/latest/pig-0.17.0.tar.gz
+tar xfz /tmp/pig.tgz
