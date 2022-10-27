73d25fa0334089a07e90f2d14e184815d7c2df35
smduarte <smd@fct.unl.pt>
2022-10-27 16:09:22 +0100

    Update install_pig.sh

--- scripts/install_pig.sh
+++ scripts/install_pig.sh
@@ -2,10 +2,8 @@
 #apt-get install -y openssh-server
 #/etc/init.d/ssh start
 
-HADOOP=hadoop-3.3.4
-HADOOP_HOME=/usr/local/$HADOOP
+echo "export JAVA_HOME="`readlink -f /usr/bin/java | sed "s:bin/java::"`
 
-curl -s https://raw.githubusercontent.com/smduarte/spbd-2223/main/scripts/install_hadoop.sh | bash
 export PIG_HOME=/opt/pig-0.17.0
 mkdir -p $PIG_HOME
 cd /opt/
